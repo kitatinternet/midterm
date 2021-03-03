d8fb6c84dc2615d3ed1c2fd0815f576cdb29d2bc
Chun Kit Lam <kitatinternet@gmail.com>
2021-03-03 18:48:30 -0500

    Update db.js
    connect to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  //"URI": "mongodb://localhost/books229"
-  "URI": "mongodb+srv://kit:TA5QNhagEFj7PNPT@mongodbserver.pefy3.mongodb.net/books229?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://kit:TA5QNhagEFj7PNPT@mongodbserver.pefy3.mongodb.net/books229?retryWrites=true&w=majority"
 };
